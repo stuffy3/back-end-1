9dba64ddfdeca5279b52054c63a1af30e4a2b84a
Tanner Stufflebeam <tannerstufflebeam@gmail.com>
2021-11-01 14:28:58 -0600

    Initialize

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/api/users', (req, res) => {
+    let friends = ["Nitin", "Eric", "Jeddy", "Cameron", "Riley"];
+    res.status(200).send(friends)
+});
+
+app.get('/weather/:tempature', (req, res) => {
+    const phrase = `<h3>It was ${req.params.temperature} today </h3>`;
+    res.status(200).send(phrase)
+})
+
+const SERVER_PORT = 4000;
+app.listen(SERVER_PORT,() => {
+    console.log(`You are jamming on ${SERVER_PORT}`);
+})
